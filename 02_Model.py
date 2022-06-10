dc14da58965bdad0daed6e9944c8950a7e7acd8d
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-06-10 23:35:09 +0200

    Added Decission Tree Model

--- 02_Model.py
+++ 02_Model.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline, make_pipeline
 import sklearn.metrics
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
 # from sklearn.metrics import confusion_matrix
 # from sklearn.metrics import classification_report
 
@@ -65,25 +67,25 @@ def Classification_Model1():
     print(metrics.accuracy_score(y_train, y_pred_train))
 
     # regularixation using randomizedSearch CV
-    from sklearn.model_selection import RandomizedSearchCV
-    random_grid = {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
-    svc_random = RandomizedSearchCV(svc, random_grid, cv=5, random_state=2020)
-    svc_random.fit(X_train, y_train)
-    print(svc_random.best_params_)
+    # from sklearn.model_selection import RandomizedSearchCV
+    # random_grid = {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+    # svc_random = RandomizedSearchCV(svc, random_grid, cv=5, random_state=2020)
+    # svc_random.fit(X_train, y_train)
+    # print(svc_random.best_params_)
 
     # using gridsearchCV
-    from sklearn.model_selection import GridSearchCV
-    param_dist = {'C': [0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4],
-                  'kernel': ['linear', 'rbf', 'poly']}
-    svc_cv = GridSearchCV(svc, param_dist, cv=10)
-    svc_cv.fit(X_train, y_train)
-    print(svc_cv.best_params_)
+    # from sklearn.model_selection import GridSearchCV
+    # param_dist = {'C': [0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4],
+    #               'kernel': ['linear', 'rbf', 'poly']}
+    # svc_cv = GridSearchCV(svc, param_dist, cv=10)
+    # svc_cv.fit(X_train, y_train)
+    # print(svc_cv.best_params_)
 
     # final SVM model (after finding best params)
-    svc_new = SVC(C=1.3, kernel="rbf", random_state=2020)
-    svc_new.fit(X_train, y_train)
-    y_pred_new = svc_new.predict(X_test)
-    print(metrics.accuracy_score(y_test, y_pred_new))
+    # svc_new = SVC(C=1.3, kernel="rbf", random_state=2020)
+    # svc_new.fit(X_train, y_train)
+    # y_pred_new = svc_new.predict(X_test)
+    # print(metrics.accuracy_score(y_test, y_pred_new))
 
 
 # Classification_Model1()
@@ -100,3 +102,6 @@ def ClassificationModel2():
     metrics.plot_confusion_matrix(dt, X_test, y_test)
     plt.show()
     print(metrics.accuracy_score(y_test, y_pred))
+
+
+ClassificationModel2()
